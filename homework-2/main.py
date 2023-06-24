8bea896b3811599869a2a0356dc9588cbe0649dc
Alex Sid <azzlem@mail.ru>
2023-06-25 01:57:01 +0300

    add func init_new_object and init many atribut

--- homework-2/main.py
+++ homework-2/main.py
@@ -12,9 +12,9 @@ if __name__ == '__main__':
     moscowpython.channel_id = 'Новое название'
     # AttributeError: property 'channel_id' of 'Channel' object has no setter
 
-    # можем получить объект для работы с API вне класса
-    print(Channel.get_service())
-    # <googleapiclient.discovery.Resource object at 0x000002B1E54F9750>
-
-    # создаем файл 'moscowpython.json' в данными по каналу
-    moscowpython.to_json('moscowpython.json')
+    # # можем получить объект для работы с API вне класса
+    # print(Channel.get_service())
+    # # <googleapiclient.discovery.Resource object at 0x000002B1E54F9750>
+    #
+    # # создаем файл 'moscowpython.json' в данными по каналу
+    # moscowpython.to_json('moscowpython.json')
--- src/channel.py
+++ src/channel.py
@@ -8,7 +8,7 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
@@ -17,8 +17,54 @@ class Channel:
 
         youtube = build('youtube', 'v3', developerKey=api_key_my_youtube)
 
-        result = youtube.channels().list(id=self.channel_id, part="snippet, statistics").execute()
+        result = youtube.channels().list(id=self.__channel_id, part="snippet, statistics").execute()
 
         result = json.dumps(result, indent=2, ensure_ascii=False)
 
         print(result)
+
+    def init_new_object(self):
+        api_key_my_youtube: str = os.getenv('API_YOUTYBE')
+        youtube = build('youtube', 'v3', developerKey="AIzaSyB48i_Psq2qTM74Yjfjy88WyKxB0DFJE84")
+        result = youtube.channels().list(id=self.__channel_id, part="snippet, statistics").execute()
+        return [
+            result["items"][0]["snippet"]["title"],
+            result["items"][0]["snippet"]["description"].split(":)")[0],
+            result["items"][0]["statistics"]["subscriberCount"],
+            result["items"][0]["statistics"]["videoCount"],
+            result["items"][0]["statistics"]["viewCount"]
+        ]
+
+    @property
+    def name_zorro(self):
+        return self.__channel_id
+
+    @property
+    def title(self):
+        result = Channel.init_new_object(self)
+        return result[0]
+
+    @property
+    def description(self):
+        result = Channel.init_new_object(self)
+        return result[1]
+
+    @property
+    def url(self):
+        return f"https://www.youtube.com/channel/{self.__channel_id}"
+
+    @property
+    def video_count(self):
+        result = Channel.init_new_object(self)
+        return result[3]
+
+    @property
+    def view_count(self):
+        result = Channel.init_new_object(self)
+        return result[4]
+
+    @property
+    def subscriber_count(self):
+        result = Channel.init_new_object(self)
+        return result[2]
+
