e881a310e8be5d65f03d002c51d3732d81814e96
Alex Sid <azzlem@mail.ru>
2023-07-08 18:37:26 +0300

    fix Video.url

--- src/video.py
+++ src/video.py
@@ -9,7 +9,7 @@ class Video:
         self.video_id = video_id
         self.title = result["items"][0]["snippet"]["title"]
         self.__channel_id = result["items"][0]["snippet"]["channelId"]
-        self.url = f"https://www.youtube.com/watch?v={self.video_id}&ab_channel{self.__channel_id}"
+        self.url = f"https://www.youtube.com/watch?v={self.video_id}"
         self.view_count = result["items"][0]["statistics"]["viewCount"]
         self.like_count = result["items"][0]["statistics"]["likeCount"]
 
