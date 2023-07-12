842cf2b5468ba23faabcb7f7b3a5baf34413a229
Alex Sid <azzlem@mail.ru>
2023-07-12 23:53:03 +0300

    add try/exept for init

--- src/video.py
+++ src/video.py
@@ -3,15 +3,21 @@ from src.channel import Channel
 
 class Video:
     """Класс для видео ютуб"""
+
     def __init__(self, video_id: str):
         """Экземпляр инициализируется id видео. Дальше все данные будут подтягиваться по API."""
         youtube = Channel.get_service()
-        result = youtube.videos().list(id=video_id, part="snippet, statistics").execute()
-        self.video_id = video_id
-        self.title: str = result["items"][0]["snippet"]["title"]
-        self.url = f"https://www.youtube.com/watch?v={self.video_id}"
-        self.view_count: int = result["items"][0]["statistics"]["viewCount"]
-        self.like_count: int = result["items"][0]["statistics"]["likeCount"]
+        try:
+            result = youtube.videos().list(id=video_id, part="snippet, statistics").execute()
+            self.video_id = video_id
+            self.title: str = result["items"][0]["snippet"]["title"]
+            self.url = f"https://www.youtube.com/watch?v={self.video_id}"
+            self.view_count: int = result["items"][0]["statistics"]["viewCount"]
+            self.like_count: int = result["items"][0]["statistics"]["likeCount"]
+        except:
+            self.video_id = video_id
+            self.like_count = None
+            self.title = None
 
     def __str__(self):
         """Возвращает строку с названием видео"""
@@ -20,9 +26,8 @@ class Video:
 
 class PLVideo(Video):
     """Класс для плейлиста видео ютуб, дочерний от Video"""
+
     def __init__(self, video_id: str, playlist_id: str):
         """Расширяет функционал класса Video добавлением id плейлиста"""
         super().__init__(video_id)
         self.channel_id = playlist_id
-
-
